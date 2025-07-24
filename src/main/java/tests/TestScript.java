cb790ad6e48106c54f736efa51e0993c59049539
rakesh-0011 <125769570+rakesh-0011@users.noreply.github.com>
2025-07-24 14:01:46 +0530

    Update TestScript.java

--- src/main/java/tests/TestScript.java
+++ src/main/java/tests/TestScript.java
@@ -1,7 +1,18 @@
-public class TestScript 
-{
-    public static void main(String[] args)
-  {
-        System.out.println("Hello World!");
+public class TestScript {
+    public static void main(String[] args) {
+        // Set up WebDriver using WebDriverManager
+        WebDriverManager.chromedriver().setup();
+        
+        // Create ChromeDriver instance
+        WebDriver driver = new ChromeDriver();
+        
+        // Open Google
+        driver.get("https://www.google.com");
+        
+        // Print page title
+        System.out.println("Page title is: " + driver.getTitle());
+        
+        // Close the browser
+        driver.quit();
     }
 }
